c42e8dc43bbcc33dda63055fe258840bceb61d36
jcdenton2030 <jcdenton2030@gmail.com>
2025-02-23 10:00:41 +1100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+// import Template from './components/Template';
+// import Template_2 from './components/Template_2';
+// import Template_3 from './components/Template_3';
+// import Template_4 from './components/Template_4';
+// import Template_5 from './components/Template_5';
+// import Template_6 from './components/Template_6';
+// import Template_7 from './components/Template_7';
+// import Template_8 from './components/Template_8';
+// import Template_9 from './components/Template_9';
+import Template_10 from './components/Template_10';
+
+
+
+function App() {
+  return (
+    <div>
+      {/* <Template /> */}
+
+      {/* <Template_2 /> */}
+
+      {/* <Template_3 /> */}
+
+      {/* <Template_4 /> */}
+
+      {/* <Template_5 /> */}
+
+      {/* <Template_6 /> */}
+
+      {/* <Template_7 /> */}
+
+      {/* <Template_8 /> */}
+
+      {/* <Template_9 /> */}
+
+      <Template_10 />
+
+    </div>
+
+  );
+}
+
+export default App;
